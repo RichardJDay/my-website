51a352f898163bd7ecb57c0f8254b46845dc9217
Richard Day <Richard.Day@outlook.com>
2025-04-07 11:04:08 +0100

    init

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import * as functions from 'firebase-functions';
+import { app } from '../src/server.ts';  // This should point to your Express server (SSR handler).
+
+export const ssr = functions.https.onRequest(app);
