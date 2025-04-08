b60e9dfc235c47152d7cbdd166e9ede2bc1da070
Richard Day <Richard.Day@outlook.com>
2025-04-08 09:23:24 +0100

    bg

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { EmailService } from '../../shared/services/email.service';
-import { HttpClient, HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import {FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-contact',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
@@ -12,12 +12,17 @@ export class ContactComponent {
   name: string = '';
   email: string = '';
   message: string = '';
-
-  constructor(private emailService: EmailService){}
+  messageSent: boolean = false;
+  constructor(private emailService: EmailService) { }
 
   submitForm() {
     console.log('Form submitted:', this.name, this.email, this.message);
-
-    this.emailService.sendEmail(this.email, this.name, this.message)
+    this.emailService.sendEmail(this.email, this.name, this.message).subscribe(() => {
+        this.name = '';
+        this.email = '';
+        this.message = '';
+        this.messageSent = true;
+      }
+    )
   }
 }
--- src/app/shared/services/email.service.ts
+++ src/app/shared/services/email.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,11 @@ export class EmailService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public sendEmail(email: string, name: string, message: string) {
-    this.httpClient.put('http://localhost:4000/email', {
+  public sendEmail(email: string, name: string, message: string): Observable<any> {
+    return this.httpClient.put('http://localhost:4000/email', {
       email: email,
       name: name,
       message: message
-    }).subscribe()
+    })
   }
 }
